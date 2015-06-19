a21ee7f63b1316413cc6c5c2b7ca20bb0a4d96c6
iLohas <iloilo_has@yahoo.co.jp>
2015-06-19 14:09:52 +0900

    assignment02のテストドライバ組み込むよ！！

--- src/assignment02/Player.java
+++ src/assignment02/Player.java
@@ -3,54 +3,72 @@ package assignment02;
 public class Player {
 
 	// じゃんけんの手
-	public static final int STONE		= 0;	// ぐー
-	public static final int SCISSORS	= 1;	// ちょき
-	public static final int PAPER		= 2;	// ぱー
+	public enum Hand{
+		STONE,
+		SCISSORS,
+		PAPER
+	}
 
 	private String name_;
-	private int winCount_;
+	private int winCount_	= 0;
 	
+	/**
+	 * コンストラクタで名前を設定する
+	 * @param name
+	 */
+	Player(String name){
+		this.name_	= name;
+	}
 	/**
 	 * ランダムに手を出す
 	 * @return playerHand
 	 */
-	public int showHand(){
+	public Hand showHand(){
 		// ジャンケンの手を決めるのに使用する乱数
 		double randomNum = 0;		
-		// プレイヤー１が出す手
-		int playerHand	= 0;
+		// プレイヤーが出す手
+		Hand playerHand	= null;
 		
 		randomNum	= Math.random() * 3;
 		if (randomNum < 1)
 		{
-			playerHand = STONE;
-			System.out.print("グー");
+			playerHand = Hand.STONE;
 		}
 		else if (randomNum < 2)
 		{
-			playerHand = SCISSORS;
-			System.out.print("チョキ");
+			playerHand = Hand.SCISSORS;
 		}
 		else if (randomNum < 3)
 		{
-			playerHand = PAPER;
-			System.out.print("パー");
+			playerHand = Hand.PAPER;
 		}
 		return playerHand;
 	}
-	
 	/**
 	 * 審判に勝敗を聞いて勝ち数を更新する
 	 */
 	public void notifyResult(){
 		
 	}
-	
 	/**
 	 * 勝った回数を数える
 	 * @return winCount_
 	 */
 	public int getWinCount(){
 		return winCount_;
+	}
+	/**
+	 * 勝った回数を設定する
+	 * @param winCount
+	 */
+	public void setWinCount(int winCount){
+		this.winCount_	= winCount;
+	}
+	/**
+	 * プレイヤーの名前を取得する
+	 * @return
+	 */
+	public String getName(){
+		return this.name_;
 	}
 }
--- src/assignment02/SampleJanken.java
+++ src/assignment02/SampleJanken.java
@@ -4,7 +4,11 @@ public class SampleJanken {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
+		Player yamada	= new Player("山田さん");
+		Player tanaka	= new Player("田中さん");
 
+		Judge satou		= new Judge();
+		satou.startJanken(yamada, tanaka);
 	}
 
 }
