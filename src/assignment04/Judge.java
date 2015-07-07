273a33f87940fbb7eb5f745c3b61bd24166aec98
iLohas <iloilo_has@yahoo.co.jp>
2015-07-07 14:33:11 +0900

    assignment04完成

--- src/assignment04/Judge.java
+++ src/assignment04/Judge.java
@@ -11,8 +11,10 @@ public class Judge {
 		
 		// じゃんけんスタートの掛け声
 		System.out.println("【じゃんけん開始】");
+		System.out.println("〜〜会話始まり〜〜");
 		System.out.println(player1.getName()+"："+"「"+player1.startWord()+"」");
 		System.out.println(player2.getName()+"："+"「"+player2.startWord()+"」");
+		System.out.println("〜〜会話終わり〜〜");
 		System.out.print("\n");
 		
 		// ３回勝負
@@ -44,7 +46,9 @@ public class Judge {
 				|| (player1Hand == Hand.SCISSORS && player2Hand == Hand.PAPER)
 				|| (player1Hand == Hand.PAPER && player2Hand == Hand.STONE)) {
 			System.out.println(player1.getName()+"が勝ちました！");
+			System.out.println("〜〜会話始まり〜〜");
 			System.out.println(player1.getName()+"："+"「"+player1.winWord()+"」");
+			System.out.println("〜〜会話終わり〜〜");
 			System.out.print("\n");
 			// プレイヤー１の勝ち数に１加える
 			player1.notifyResult();
@@ -54,15 +58,19 @@ public class Judge {
 				|| (player1Hand == Hand.SCISSORS && player2Hand == Hand.STONE)
 				|| (player1Hand == Hand.PAPER && player2Hand == Hand.SCISSORS)) {
 			System.out.println(player2.getName()+"が勝ちました！");
+			System.out.println("〜〜会話始まり〜〜");
 			System.out.println(player2.getName()+"："+"「"+player2.winWord()+"」");
+			System.out.println("〜〜会話終わり〜〜");
 			System.out.print("\n");
 			player2.notifyResult();
 		}
 		// 引き分けの場合
 		else {
 			System.out.println("引き分けです！");
+			System.out.println("〜〜会話始まり〜〜");
 			System.out.println(player1.getName()+"："+"「"+player1.drawWord()+"」");
 			System.out.println(player2.getName()+"："+"「"+player2.drawWord()+"」");
+			System.out.println("〜〜会話終わり〜〜");
 			System.out.print("\n");
 		}
 	}
@@ -73,20 +81,33 @@ public class Judge {
 	 * @return player（勝ち数が同じ場合はnull）
 	 */
 	public void judgeFinalWinner(Player player1, Player player2){
+		// じゃんけん終わりの掛け声
+		System.out.println("【じゃんけん終了】");
+		
 		// プレイヤー１の勝ち数が多い時
 		if (player1.getWinCount() > player2.getWinCount())
 		{
-			System.out.println(player1.getWinCount()+" 対 "+player2.getWinCount()+"で"+player1.getName()+"の勝ちです！\n");
+			System.out.println(player1.getWinCount()+" 対 "+player2.getWinCount()+"で"+player1.getName()+"の勝ちです！");
+			System.out.println("〜〜会話始まり〜〜");
+			System.out.println(player1.getName()+"："+"「"+player1.winWord()+"」");
+			System.out.println("〜〜会話終わり〜〜");
 		}
 		// プレイヤー２の勝ち数が多い時
 		else if (player1.getWinCount() < player2.getWinCount())
 		{
-			System.out.println(player1.getWinCount()+" 対 "+player2.getWinCount()+"で"+player2.getName()+"の勝ちです！\n");
+			System.out.println(player1.getWinCount()+" 対 "+player2.getWinCount()+"で"+player2.getName()+"の勝ちです！");
+			System.out.println("〜〜会話始まり〜〜");
+			System.out.println(player2.getName()+"："+"「"+player2.winWord()+"」");
+			System.out.println("〜〜会話終わり〜〜");
 		}
 		// プレイヤー１と２の勝ち数が同じ時
 		else
 		{
-			System.out.println(player1.getWinCount()+" 対 "+player2.getWinCount()+"で引き分けです！\n");
+			System.out.println(player1.getWinCount()+" 対 "+player2.getWinCount()+"で引き分けです！");
+			System.out.println("〜〜会話始まり〜〜");
+			System.out.println(player1.getName()+"："+"「"+player1.drawWord()+"」");
+			System.out.println(player2.getName()+"："+"「"+player2.drawWord()+"」");
+			System.out.println("〜〜会話終わり〜〜");
 		}
 	}	
 	/**
--- src/assignment04/ObjectJanken.java
+++ src/assignment04/ObjectJanken.java
@@ -4,10 +4,10 @@ public class ObjectJanken {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-		Player yamada	= new Player("山田さん");
-		Player tanaka	= new Player("田中さん");
+		Yamada yamada	= new Yamada("山田さん");
+		Murata murata	= new Murata("村田さん");
 
 		Judge judgemen		= new Judge();
-		judgemen.startJanken(yamada, tanaka);
+		judgemen.startJanken(yamada, murata);
 	}
 }
